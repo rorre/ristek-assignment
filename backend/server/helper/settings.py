fcda03aedf4bfc1b931ee079603808ac377a47bd
Rendy Arya Kemal <renrror@gmail.com>
2022-03-12 21:19:33 +0700

    Add workaround for postgres URL in heroku

--- backend/server/helper/settings.py
+++ backend/server/helper/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, root_validator
 
 
 class Settings(BaseSettings):
@@ -9,6 +9,13 @@ class Settings(BaseSettings):
     database_url: str
     secret: str
     frontend_url: str
+
+    @root_validator
+    def _fix_postgres_url_heroku(cls, values):
+        db_url = values.get("database_url", None) or values.get("DATABASE_URL", None)
+        if db_url.startswith("postgres://"):
+            values["database_url"] = db_url.replace("postgres://", "postgresql://", 1)
+        return values
 
 
 settings = Settings()
