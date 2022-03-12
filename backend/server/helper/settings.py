04e4979ff1f854354406e2d7c12b105b733be953
Rendy Arya Kemal <renrror@gmail.com>
2022-03-12 20:48:06 +0700

    Add gunicorn + proper settings config

--- backend/server/helper/settings.py
+++ backend/server/helper/settings.py
@@ -2,8 +2,12 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
     database_url: str
     secret: str
 
 
-settings = Settings(".env")
+settings = Settings()
