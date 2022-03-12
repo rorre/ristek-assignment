f5f29848d79df8e1b4f5d3fcad50fced00b49467
Rendy Arya Kemal <renrror@gmail.com>
2022-03-12 13:22:40 +0700

    Wrap app around UserContext

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "components/elements/Navbar";
+import { UserContextProvider } from "components/context/UserContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserContextProvider>
       <Navbar />
       <Component {...pageProps} />;
-    </>
+    </UserContextProvider>
   );
 }
 
