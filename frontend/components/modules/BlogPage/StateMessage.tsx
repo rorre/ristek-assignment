ec39b31de517e73db7f432af1abe29d53f3fd07a
Rendy Arya Kemal <renrror@gmail.com>
2022-03-13 15:34:13 +0700

    Engwish

--- frontend/components/modules/BlogPage/StateMessage.tsx
+++ frontend/components/modules/BlogPage/StateMessage.tsx
@@ -30,7 +30,7 @@ const StateMessage: React.FC<StateMessageProps> = ({
       {showEmpty && (
         <>
           <AiOutlineQuestion className="w-12 h-12 stroke-2 text-white" />
-          <span className="text-white">Belum ada apa-apa di sini!</span>
+          <span className="text-white">There&apos;s nothing here!!</span>
         </>
       )}
     </div>
